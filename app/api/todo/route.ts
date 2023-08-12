470ec5df2708fa097fcca1e7a4eae9843e987069
Abdullah Khetran <khetranabdullah@gmail.com>
2023-08-12 21:08:44 +0500

    refactored code for drizzle

--- app/api/todo/route.ts
+++ app/api/todo/route.ts
@@ -1,19 +1,19 @@
-import { db } from "@vercel/postgres"
+import { todoTable, Todo, NewTodo, db } from "@/lib/drizzle";
+// import { db } from "@vercel/postgres"
 import { NextRequest, NextResponse } from "next/server"
-
+import { sql } from "@vercel/postgres"
 
 export async function GET(request: NextRequest) {
-    const client = await db.connect();
 
     try {
-
-        await client.sql`CREATE TABLE IF NOT EXISTS Todos(id  serial, Task varchar(255));`
-        const res = await client.sql`SELECT * FROM Todos`
-        console.log(res.rows.find((item) => item.id === 1))
+        await sql`CREATE TABLE IF NOT EXISTS Todos(id  serial, Task varchar(255));`
+        const res = await db.select().from(todoTable)
+        console.log(res)
         return NextResponse.json({ res })
+
     } catch (err) {
         console.log(err)
-        // return new NextResponse("Something went wrong")
+        console.log((err as { message: string }).message)
         return NextResponse.json("Something went wrong")
     }
 
@@ -21,11 +21,13 @@ export async function GET(request: NextRequest) {
 }
 
 export async function POST(request: NextRequest) {
-    const client = await db.connect();
     const req = await request.json();
     try {
         if (req.task) {
-            const res = await client.sql`INSERT INTO Todos(Task) VALUES(${req.task})`
+            // const res = await sql`INSERT INTO Todos(Task) VALUES(${req.task})`
+            const res = await db.insert(todoTable).values({
+                task: req.task
+            }).returning();
             console.log(res)
             return NextResponse.json({ message: "Data added sucessfully" })
         } else {
