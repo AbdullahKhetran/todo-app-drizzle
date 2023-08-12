f465b889085af7137c48b7f534c9f37c74d2d4e6
Abdullah Khetran <khetranabdullah@gmail.com>
2023-08-12 21:08:08 +0500

    connected drizzle

---
+++ lib/drizzle.ts
@@ -0,0 +1,20 @@
+import { InferModel } from 'drizzle-orm'
+import {
+    pgTable,
+    serial,
+    varchar,
+} from 'drizzle-orm/pg-core'
+import { drizzle } from "drizzle-orm/vercel-postgres"
+import { sql } from "@vercel/postgres"
+
+export const todoTable = pgTable("todos", {
+    id: serial("id").primaryKey().notNull(),
+    task: varchar("task", { length: 255 }).notNull()
+})
+
+// for getting data
+export type Todo = InferModel<typeof todoTable>;
+// for inserting data
+export type NewTodo = InferModel<typeof todoTable, "insert">
+
+export const db = drizzle(sql)
