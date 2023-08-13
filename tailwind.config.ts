d9efb602162d70f243f804844156a49d8c2abd47
Abdullah Khetran <khetranabdullah@gmail.com>
2023-08-13 21:23:32 +0500

    added custom colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,10 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+      colors: {
+        "pink": "#DD2476",
+        "orange": "#FF512F",
+      }
     },
   },
   plugins: [],
