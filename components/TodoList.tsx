ea8440dbe845e2ee210655964e2e5325058e0763
Abdullah Khetran <khetranabdullah@gmail.com>
2023-08-14 00:44:47 +0500

    testing fetch on deploy

--- components/TodoList.tsx
+++ components/TodoList.tsx
@@ -2,72 +2,72 @@ require('dotenv').config
 import { Todo } from "@/lib/drizzle";
 
 
-// const getData = async (): Promise<Todo[]> => {
+const getData = async (): Promise<Todo[]> => {
 
-//     // in server components absolute url path is needed so will construct that
+    // in server components absolute url path is needed so will construct that
 
-//     const deployedURL = process.env.REACT_APP_API_URL
+    const deployedURL = process.env.REACT_APP_API_URL
 
-//     let isLocalHost: boolean = true
-//     // if (typeof window === undefined) {
-//     //     isLocalHost = true
-//     // }
+    let isLocalHost: boolean = false
+    // if (typeof window === undefined) {
+    //     isLocalHost = true
+    // }
 
-//     const siteUrl = isLocalHost ? "http://localhost:3000" : deployedURL
+    const siteUrl = isLocalHost ? "http://localhost:3000" : deployedURL
 
-//     try {
-//         const res = await fetch(`${siteUrl}/api/todo`, {
-//             method: "GET",
-//             cache: "no-store",
-//             headers: {
-//                 "Content-Type": "application/json"
-//             }
-//         });
-//         if (!res.ok) {
-//             throw new Error("Failed to fetch the data")
-//         };
-//         const result: Todo[] = await res.json()
+    try {
+        const res = await fetch(`${siteUrl}/api/todo`, {
+            method: "GET",
+            cache: "no-store",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        });
+        if (!res.ok) {
+            throw new Error("Failed to fetch the data")
+        };
+        const result: Todo[] = await res.json()
 
-//         return result
-//     } catch (err) {
-//         // console.log(err)
-//         throw err
-//     }
+        return result
+    } catch (err) {
+        // console.log(err)
+        throw err
+    }
 
-// }
+}
 
-// const TodoList = async () => {
+const TodoList = async () => {
 
-//     const res = await getData();
+    const res = await getData();
 
-//     return (
-
-//         <div className="max-h-[350px] overflow-auto mb-4 ">
-
-//             {
-//                 res.map((item) => {
-//                     return (
-//                         <div key={item.id} className="bg-gray-100 py-4 px-4 flex items-center gap-x-3 shadow rounded-lg my-5">
-//                             {/* Circle */}
-//                             <div className="h-3 w-3 bg-orange rounded-full"></div>
-//                             {/* Task Title */}
-//                             <p className="text-lg font-medium">{item.task}</p>
-//                         </div>
-//                     )
-//                 })
-//             }
-
-//         </div>
-//     )
-// }
-
-
-// export default TodoList
-
-const Temp = () => {
     return (
-        <div>Testing</div>
+
+        <div className="max-h-[350px] overflow-auto mb-4 ">
+
+            {
+                res.map((item) => {
+                    return (
+                        <div key={item.id} className="bg-gray-100 py-4 px-4 flex items-center gap-x-3 shadow rounded-lg my-5">
+                            {/* Circle */}
+                            <div className="h-3 w-3 bg-orange rounded-full"></div>
+                            {/* Task Title */}
+                            <p className="text-lg font-medium">{item.task}</p>
+                        </div>
+                    )
+                })
+            }
+
+        </div>
     )
 }
 
-export default Temp
+
+export default TodoList
+
+// const Temp = () => {
+//     return (
+//         <div>Testing</div>
+//     )
+// }
+
+// export default Temp
