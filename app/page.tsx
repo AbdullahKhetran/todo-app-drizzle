3140008f33ee0fbb56ccb6a1baa5326058b2c84c
Abdullah Khetran <khetranabdullah@gmail.com>
2023-08-14 00:49:16 +0500

    minor change

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
       <div className='px-6 py-8 rounded-xl bg-gradient-to-br from-[#D9D9D9]/50 to-[#D9D9D9]/60  backdrop-blur-xl w-full max-w-md'>
 
 
-        {/* <TodoList /> */}
+        <TodoList />
 
         <AddTodo />
 
