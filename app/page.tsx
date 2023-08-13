d665759fa514b0c9aa2f997a3d820c30cd67b178
Abdullah Khetran <khetranabdullah@gmail.com>
2023-08-13 21:24:02 +0500

    added forntend

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,21 @@
+import AddTodo from "@/components/AddTodo"
+import TodoList from "@/components/TodoList"
+
 export default function Home() {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <main className='bg-gradient-to-tr from-pink to-orange h-screen
+    flex justify-center items-center'>
+      <div className='px-6 py-8 rounded-xl bg-gradient-to-br from-[#D9D9D9]/50 to-[#D9D9D9]/60  backdrop-blur-xl w-full max-w-md'>
+
+
+        <TodoList />
+
+        <AddTodo />
+
+        {/* Bar */}
+        <div className='w-1/2 h-1.5 bg-black/80 rounded mx-auto mt-6'></div>
+      </div>
+    </main>
   )
 
 }
